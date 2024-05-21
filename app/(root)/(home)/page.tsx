b1d176d3538df92310fc4d2d8fca54379e05c154
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2024-05-21 17:35:53 +0530

    feat: add clerk auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,16 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+export default function Home() {
+  return (
+    <div>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>
+    </div>
+  );
+}
