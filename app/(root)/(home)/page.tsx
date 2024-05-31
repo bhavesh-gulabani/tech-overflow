a7b9268e0a277ec72db1ef71ded1e9f3bc75a1cd
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2024-05-31 20:00:21 +0530

    feat: add clerk webhook for user creation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,8 +12,6 @@ import { getQuestions } from '@/lib/actions/question.action';
 const Home = async () => {
   const result = await getQuestions({});
 
-  console.log(result.questions);
-
   return (
     <>
       <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
