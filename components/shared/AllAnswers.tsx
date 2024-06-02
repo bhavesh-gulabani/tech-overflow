c4fab41ad51384fa5a9d387d713053ff95307bbb
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2024-06-02 16:21:30 +0530

    feat: add question details and answers

---
+++ components/shared/AllAnswers.tsx
@@ -0,0 +1,63 @@
+import { AnswerFilters } from '@/constants/filters';
+import Filter from './Filter';
+import { getAnswers } from '@/lib/actions/answer.action';
+import Link from 'next/link';
+import Image from 'next/image';
+import { getTimestamp } from '@/lib/utils';
+import ParseHTML from './ParseHTML';
+
+interface Props {
+  questionId: string;
+  authorId: string;
+  totalAnswers: number;
+  page?: number;
+  filter?: number;
+}
+
+const AllAnswers = async ({ questionId, userId, totalAnswers }: Props) => {
+  const result = await getAnswers({ questionId: JSON.parse(questionId) });
+  console.log(userId);
+
+  return (
+    <div className='mt-11'>
+      <div className='flex items-center justify-between'>
+        <h3 className='primary-text-gradient'>{totalAnswers} Answers</h3>
+        <Filter filters={AnswerFilters} />
+      </div>
+
+      <div>
+        {result.answers.map((answer) => (
+          <article key={answer._id} className='light-border border-b py-10'>
+            <div className='flex items-center justify-between'>
+              <div className='mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+                <Link
+                  href={`/profile/${answer.author.clerkId}`}
+                  className='flex flex-1 items-start gap-1 sm:items-center'
+                >
+                  <Image
+                    src={answer.author.picture}
+                    width={18}
+                    height={18}
+                    alt='Profile'
+                    className='rounded-full object-cover max-sm:mt-0.5'
+                  />
+                  <div className='flex flex-col sm:flex-row sm:items-center'>
+                    <p className='body-semibold text-dark300_light700 '>
+                      {answer.author.name}
+                    </p>
+                    <p className='small-regular text-light400_light500 ml-1 mt-0.5 line-clamp-1'>
+                      answered {getTimestamp(answer.createdAt)}
+                    </p>
+                  </div>
+                </Link>
+                <div className='flex justify-end'>VOTING</div>
+              </div>
+            </div>
+            <ParseHTML data={answer.content} />
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default AllAnswers;
