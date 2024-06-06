1e1c5b9d315bb16f1087ec8617b530584825ecf4
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2024-06-06 10:54:35 +0530

    feat: implement top results in sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,40 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import RenderTag from './RenderTag';
+import { getHotQuestions } from '@/lib/actions/question.action';
+import { getTopPopularTags } from '@/lib/actions/tag.action';
 
-const hotQuestions = [
-  {
-    _id: '1',
-    title:
-      'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
-  },
-  {
-    _id: '2',
-    title: 'Is it only me or the font is bolder than necessary?',
-  },
-  {
-    _id: '3',
-    title: 'Redux Toolkit Not Updating State as Expected',
-  },
-  {
-    _id: '4',
-    title: 'Async/Await Function Not Handling Errors Properly',
-  },
-  {
-    _id: '5',
-    title: 'How to Perfectly Center a Div with Tailwind CSS?',
-  },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
-const popularTags = [
-  { _id: '1', name: 'javascript', totalQuestions: 5 },
-  { _id: '2', name: 'react', totalQuestions: 4 },
-  { _id: '3', name: 'next', totalQuestions: 8 },
-  { _id: '4', name: 'vue', totalQuestions: 2 },
-  { _id: '5', name: 'redux', totalQuestions: 3 },
-];
-
-const RightSidebar = () => {
   return (
     <section className='background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]'>
       <div>
@@ -43,7 +16,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className='flex cursor-pointer items-center justify-between gap-7'
             >
               <p className='body-medium text-dark500_light700'>
@@ -68,7 +41,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
